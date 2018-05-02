ee79ef9ebb1353738797cdac79b797341e637d54
River Gillis <jrivergillis@gmail.com>
2018-05-02 15:03:29 -0500

    finish minimal tree

--- 4-trees-graphs/2_minimal_tree.py
+++ 4-trees-graphs/2_minimal_tree.py
@@ -3,7 +3,7 @@
 # hints 19, 73, 116
 
 class Node(object):
-    def __init__(self, item, left=None, right=None):
+    def __init__(self, item=None, left=None, right=None):
         self.item = item
         self.left = left
         self.right = right
@@ -18,9 +18,22 @@ class Node(object):
             total.append(', ')
             total.append(str(self.right))
         return ''.join(total)
+    
+    def __repr__(self):
+        return str(self);
 
+# This is sort of like a binary search in reverse
 def minimal_tree(a_list):
-    pass
+    if not a_list:
+        return None
+    tree = Node()
+    midpoint = len(a_list) // 2
+    tree.item = Node(a_list[midpoint])
+    if len(a_list) > 1:
+        tree.left = minimal_tree(a_list[0:midpoint])
+        tree.right = minimal_tree(a_list[midpoint+1:len(a_list)])
+    return tree
+
 
 if __name__ == '__main__':
     a = [0, 1, 5, 9, 10]
